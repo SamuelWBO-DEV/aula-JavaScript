f3a06cde38aa453977ff0074e4b2ff6bfe951d1e
SamuelWBO-DEV <samuelw050209@gmail.com>
2024-05-07 18:11:14 -0300

    Objeto*

--- Javascript.js
+++ Javascript.js
@@ -1,16 +1,9 @@
-let primeiraNota=0
-let segundaNota= 0
-let terceiraNota= 0
-let media= 0
+let objetoUsuario = {
+    nome: "Samuel",
+    idade: 29,
+    genero:"Masculino",
+    cor:"Branco",
+    corFavorita: ["azul","red","preto"]
+}
 
-primeiraNota= prompt("digite sua primeira nota")
-segundaNota= prompt("digite sua segunda nota")
-terceiraNota= prompt("digite sua terceira nota")                 
-
-primeiraNota= parseInt(primeiraNota)
-segundaNota= parseInt(segundaNota)
-terceiraNota= parseInt(terceiraNota)
-
-media= (primeiraNota + segundaNota + terceiraNota) /3
- alert("sua média é: " + media)
-
+console.log(objetoUsuario)
