26fff28c5c8b11013f18f50ddc60ea79223f7624
SamuelWBO-DEV <samuelw050209@gmail.com>
2024-05-08 13:53:06 -0300

    Eventos

--- Javascript.js
+++ Javascript.js
@@ -1,16 +1,25 @@
-function load (){
-    let Nascimento = document.getElementById("input")
-
-    document.getElementById ("btn").onclick =function(){
-        let resultado = 2024 - parseInt (Nascimento.value)
-        document.getElementById("idade").innerText = resultado 
-    }
+function alertbtn() {
+    //alert("Consegui")
+    let input= document.getElementById("input");
+    document.body.style.backgroundColor= input.value;
 }
+
+
+
+//function capturaDados(valor) {
+    //console.log(valor.value);
+//}
+
+function main() {
+    //let input= document.getElementById("input");
+    //input.onkeyup= function (objeto) {
+    //    console.log(objeto.currentTarget.value);
+   // }
+}
+
 window.onload = function(){
-    load()
+    main()
 }
-
-
 
 
 
