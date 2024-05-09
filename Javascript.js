a6c5dc6ae2e4b5d520422dfeef0f16fea251e3b5
SamuelWBO-DEV <samuelw050209@gmail.com>
2024-05-08 23:17:42 -0300

    Calculadora

--- Javascript.js
+++ Javascript.js
@@ -1,24 +1,26 @@
-function alertbtn() {
-    //alert("Consegui")
-    let input= document.getElementById("input");
-    document.body.style.backgroundColor= input.value;
-}
+function calculadora(){
+    let resultado= document.getElementById("resultado");
+    let resultadoCalculo= 0;
+    let operacoes= document.getElementById("operacoes").value;
+    let input1 = parseInt(document.getElementById("input1").value);
+    let input2 = parseInt(document.getElementById("input2").value);
+    
+   if (operacoes=="+") {
+    resultadoCalculo=input1+input2
+   }
 
+   if (operacoes=="-") {
+    resultadoCalculo=input1-input2
+   }
 
+   if (operacoes=="*") {
+    resultadoCalculo=input1*input2
+   }
+   if (operacoes=="/") {
+    resultadoCalculo=input1/input2
+   }
 
-//function capturaDados(valor) {
-    //console.log(valor.value);
-//}
-
-function main() {
-    //let input= document.getElementById("input");
-    //input.onkeyup= function (objeto) {
-    //    console.log(objeto.currentTarget.value);
-   // }
-}
-
-window.onload = function(){
-    main()
+   resultado.innerText= resultadoCalculo
 }
 
 
