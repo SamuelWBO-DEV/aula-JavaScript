c94b6b24b8b417e150c6cfd8bb7b66326a487789
SamuelWBO-DEV <samuelw050209@gmail.com>
2024-05-30 10:02:55 -0300

    API TESTE

--- Javascript.js
+++ Javascript.js
@@ -1,20 +1,27 @@
-function inserirNome() {
-    const inputNome = document.querySelector("input").value;
-    const divInseri = document.createElement("div");
-    divInseri.className = "nome";
-    divInseri.innerText = inputNome;
-    document.querySelector("#container").appendChild(divInseri)
+async function userInfor() {
+    const result = await fetch("https://randomuser.me/api/?results=50");
+    const user = await result.json();
+
+    user.results.forEach((element) => {
+        const data = new Date(element.dob.date)
+        const divNome = document.createElement("div");
+        const divEmail = document.createElement("div");
+        const img = document.createElement("img");
+        const divData = document.createElement("div");
+        divEmail.classList.add("nome")
+        divNome.classList.add("nome");
+        img.src = element.picture.large;
+        divNome.innerText = element.name.first;
+        divEmail.innerText = element.email;
+        divData.innerText = data.toLocaleDateString();
+        document.querySelector("#container").appendChild(divNome);
+        document.querySelector("#container").appendChild(divEmail);
+        document.querySelector("#container").appendChild(img);
+        document.querySelector("#container").appendChild(divData);
+    });
+    console.log(user.results[0])
 }
 
-function armazenNomes() {
-    const dadosSalvos =  document.querySelector("#container").innerHTML;
-    localStorage.setItem("nomes-salvos", dadosSalvos);
-}
-
-function armazenCarregamento() {
-    document.querySelector("#container").innerHTML = localStorage.getItem("nomes-salvos")
-}
-
-    window.addEventListener("load", ()=>{
-        armazenCarregamento()
-    })
+window.addEventListener("load",()=>{
+    userInfor()
+})
